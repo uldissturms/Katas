73cdcb08c83c1775798b2153882fbe1bfed7baae
Uldis Sturms <uldis.sturms@gmail.com>
2017-02-04 00:43:14 +0000

    more ramda

--- CheckoutKata_JS/checkout.js
+++ CheckoutKata_JS/checkout.js
@@ -11,46 +11,42 @@ const discounts = {
   papaya: {count: 3, amount: 0.50}
 }
 
-module.exports = items =>
-  R.pipe(
-    R.map(priceItem),
-    toBasket,
-    subTotal,
-    discount,
-    total)(items)
-
-const priceItem = item => ({
-  name: item,
-  price: price(item)
+const price = R.flip(R.prop)(pricing)
+const priceItem = R.applySpec({
+  name: R.identity,
+  price
 })
 
-const price = item => pricing[item]
+const sumByPrice = R.pipe(
+  R.prop('items'),
+  R.map(R.prop('price')),
+  R.sum
+)
+const toSummary = (value, key) => ({name: key, count: value.length})
+const hasDiscount = R.pipe(R.prop('name'), R.flip(R.has)(discounts))
+const discountFor = ({name, count}) =>
+  Math.floor(count / discounts[name].count) * discounts[name].amount
+
+const calculateDiscount = R.pipe(
+  R.prop('items'),
+  R.filter(hasDiscount),
+  R.groupBy(R.prop('name')),
+  R.mapObjIndexed(toSummary),
+  R.map(discountFor),
+  R.values,
+  R.sum
+)
+
+const priceItems = R.map(priceItem)
 const toBasket = items => ({items})
+const subTotal = basket => R.assoc('subTotal', sumByPrice(basket))(basket)
+const discount = basket => R.assoc('discount', calculateDiscount(basket))(basket)
+const total = basket => R.assoc('total', basket.subTotal - basket.discount)(basket)
 
-const extendBasket = fn => basket =>
-  Object.assign({}, basket, fn(basket))
-
-const subTotal = extendBasket(({items}) =>
-  ({subTotal: sumByPrice(items)}))
-
-const sumByPrice = items =>
-  R.pipe(
-    R.map(R.prop('price')),
-    R.sum)(items)
-
-const discount = extendBasket(({items}) =>
-  ({discount: R.pipe(
-    R.filter(hasDiscount),
-    R.groupBy(R.prop('name')),
-    R.toPairs,
-    R.map(discountFor),
-    R.sum
-    )(items)}))
-
-const hasDiscount = item => R.has(item.name)(discounts)
-const discountFor = group =>
-  Math.floor(group[1].length / discounts[group[0]].count) * discounts[group[0]].amount
-
-const total = extendBasket(({subTotal, discount}) =>
-  ({total: subTotal - discount}))
-
+module.exports = R.pipe(
+  priceItems,
+  toBasket,
+  subTotal,
+  discount,
+  total
+)
